6427e19056991a9195d50e1289061516e187f316
Plaza4756 <305116929+Plaza4756@users.noreply.github.com>
2026-07-14 22:31:53 +0000

    Added BACKEND_PORT env variable

--- rxconfig.py
+++ rxconfig.py
@@ -4,12 +4,15 @@ import os
 
 proxy_content = os.environ.get("PROXY_CONTENT", "TRUE").upper() == "TRUE"
 socks5 = os.environ.get("SOCKS5", "")
+backend_port = os.environ.get("BACKEND_PORT", "8004")
 
 print(f"PROXY_CONTENT: {proxy_content}\nSOCKS5: {socks5}")
 
 config = rx.Config(
     app_name="StepDaddyLiveHD",
     proxy_content=proxy_content,
+    backend_port=int(backend_port),
+    telemetry_enabled=False,
     socks5=socks5,
     show_built_with_reflex=False,
     plugins=[
