cca5981840a303d0ffff9a97506e741762b97331
Plaza4756 <305116929+Plaza4756@users.noreply.github.com>
2026-08-18 13:49:02 +0000

    Update latest DLHD base url

--- StepDaddyLiveHD/step_daddy.py
+++ StepDaddyLiveHD/step_daddy.py
@@ -39,7 +39,7 @@ class StepDaddy:
         else:
             self._session = AsyncSession(
                 impersonate="chrome146", retry=strategy, allow_redirects="safe")
-        self._base_url = "https://dlhd.st"
+        self._base_url = "https://dlstreams.st"
         self.channels = []
         with open("StepDaddyLiveHD/meta.json", "r") as f:
             self._meta = json.load(f)
