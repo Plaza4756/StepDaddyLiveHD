590d142806ca320aa8ff66ce9cb5433096c658d8
Plaza4756 <305116929+Plaza4756@users.noreply.github.com>
2026-09-25 02:36:23 +0000

    Implement de-obfucated js logic locally. Remove cloakbrowser dependency (#6)
    * remove cdp_host
    
    * remove playwright depedency as cloakbrowser is no longer needed
    
    * Remove cloakbrowser sidecar as it is no longer needed
    
    * Use PORT env in health check
    
    * remove cdp_host
    
    * remove cloakbrowser requirement and implement de-obfuscated js logic to extract m3u8 playlist url
    
    * Remove cloakbrowser mention as the de-obfuscated js logic is handled locally

--- rxconfig.py
+++ rxconfig.py
@@ -5,7 +5,6 @@ import os
 proxy_content = os.environ.get("PROXY_CONTENT", "TRUE").upper() == "TRUE"
 socks5 = os.environ.get("SOCKS5", "")
 backend_port = os.environ.get("BACKEND_PORT", "8004")
-cdp_host = os.environ.get("CDP_HOST", "http://cloakbrowser:9222")
 
 print(f"PROXY_CONTENT: {proxy_content}\nSOCKS5: {socks5}")
 
@@ -15,7 +14,6 @@ config = rx.Config(
     backend_port=int(backend_port),
     telemetry_enabled=False,
     socks5=socks5,
-    cdp_host=cdp_host,
     show_built_with_reflex=False,
     plugins=[
         rx.plugins.SitemapPlugin(),
